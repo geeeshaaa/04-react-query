97890a0a7ce794e01dccdf195785acd34a6cb612
GennadiyS <geeeshaaa@gmail.com>
2026-03-14 13:52:12 +0200

    fix 4

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -6,7 +6,7 @@ import type { Movie } from '../types/movie';
 
 
 const instance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: 'https://api.themoviedb.org/3/',
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     Accept: 'application/json',
